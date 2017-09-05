71cfa68a37da6f24e3b3727443e619b24d5bd3f8
Sean Boots <sean.boots@gmail.com>
2017-09-04 20:45:12 -0400

    ESDC handler (fortunately, pretty much identical to Service Canada's!)

---
+++ app/DepartmentHandlers/EsdcHandler.php
@@ -0,0 +1,71 @@
+<?php
+namespace App\DepartmentHandlers;
+
+use App\DepartmentHandler;
+use App\Helpers;
+
+class EsdcHandler extends DepartmentHandler {
+
+	public $indexUrl = 'http://disclosure.esdc.gc.ca/dp-pd/prdlstcdn-eng.jsp?site=1&section=2';
+	public $baseUrl = 'http://disclosure.esdc.gc.ca/';
+	public $ownerAcronym = 'esdc';
+
+	// From the index page, list all the "quarter" URLs
+	public $indexToQuarterXpath = "//main//ul/li/a/@href";
+
+	public $multiPage = 0;
+
+	public $quarterToContractXpath = "//main//table//td//a/@href";
+
+	public function quarterToContractUrlTransform($contractUrl) {
+		return "http://disclosure.esdc.gc.ca/dp-pd/" . $contractUrl;
+	}
+
+	public function indexToQuarterUrlTransform($url) {
+		return "http://disclosure.esdc.gc.ca/dp-pd/" . $url;
+	}
+
+	public $contractContentSubsetXpath = "//main";
+
+	public function fiscalYearFromQuarterPage($quarterHtml) {
+
+		// <h1 id="wb-cont" property="name" class="page-header mrgn-tp-md">2016-2017, 3rd quarter (1 October - 31 December 2016)</h1>
+
+		return Helpers::xpathRegexComboSearch($quarterHtml, "//h2", '/([0-9]{4})/');
+
+	}
+
+	public function fiscalQuarterFromQuarterPage($quarterHtml) {
+
+		return Helpers::xpathRegexComboSearch($quarterHtml, "//h2", '/\s([0-9])</');
+
+	}
+
+	public static function parseHtml($html) {
+
+		// Service Canada doesn't include an "original contract value" on their entries. Only the current contract value is listed.
+
+		$keyArray = [
+			'vendorName' => 'Vendor Name:',
+			'referenceNumber' => 'Reference Number:',
+			'contractDate' => 'Contract Date:',
+			'description' => 'Description of work:',
+			'extraDescription' => 'Description (more details):',
+			'contractPeriodStart' => '',
+			'contractPeriodEnd' => '',
+			'contractPeriodRange' => 'Contract Period:',
+			'deliveryDate' => 'Delivery Date:',
+			'originalValue' => '',
+			'contractValue' => 'Current Contract Value:',
+			'comments' => 'Comments:',
+		];
+
+	    $values = Helpers::genericXpathParser($html, "//table//th", "//table//td", 'to', $keyArray);
+
+	    
+
+	    return $values;
+
+	}
+
+}
