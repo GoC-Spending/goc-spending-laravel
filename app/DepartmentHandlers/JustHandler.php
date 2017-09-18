adceae5ea3db3831a45aa3df58cb47a31aa301bf
Sean Boots <sean.boots@gmail.com>
2017-09-18 00:11:15 -0400

    Added Justice handler

---
+++ app/DepartmentHandlers/JustHandler.php
@@ -0,0 +1,69 @@
+<?php
+namespace App\DepartmentHandlers;
+
+use App\DepartmentHandler;
+use App\Helpers;
+
+class JustHandler extends DepartmentHandler {
+
+	public $indexUrl = 'http://www.justice.gc.ca/eng/trans/pd-dp/contra/rep-rap.asp';
+	public $baseUrl = 'http://www.justice.gc.ca/';
+	public $ownerAcronym = 'just';
+
+	// From the index page, list all the "quarter" URLs
+	public $indexToQuarterXpath = "//main//ul/li/a/@href";
+
+	public $multiPage = 0;
+
+	public $quarterToContractXpath = "//main//table//td//a/@href";
+
+	
+	public function quarterToContractUrlTransform($contractUrl) {
+		return "http://www.justice.gc.ca/eng/trans/pd-dp/contra/" . $contractUrl;
+	}
+	
+
+	
+	public function indexToQuarterUrlTransform($url) {
+		return "http://www.justice.gc.ca/eng/trans/pd-dp/contra/" . $url;
+	}
+	
+
+	public $contractContentSubsetXpath = "//main";
+
+	public function fiscalYearFromQuarterPage($quarterHtml) {
+
+		// <h1 id="wb-cont" property="name" class="page-header mrgn-tp-md">2016-2017, 3rd quarter (1 October - 31 December 2016)</h1>
+
+		return Helpers::xpathRegexComboSearch($quarterHtml, "//main//h2", '/([0-9]{4})/');
+
+	}
+
+	public function fiscalQuarterFromQuarterPage($quarterHtml) {
+
+		return Helpers::xpathRegexComboSearch($quarterHtml, "//main//h2", '/([0-9])</');
+
+	}
+
+	public static function parseHtml($html) {
+
+		$keyArray = [
+			'vendorName' => 'Vendor Name:',
+			'referenceNumber' => 'Reference Number:',
+			'contractDate' => 'Contract Date:',
+			'description' => 'Description of work:',
+			'extraDescription' => 'Additional Comments:',
+			'contractPeriodStart' => '',
+			'contractPeriodEnd' => '',
+			'contractPeriodRange' => 'Contract Period:',
+			'deliveryDate' => 'Delivery Date:',
+			'originalValue' => 'Original Contract Value:',
+			'contractValue' => 'Contract Value:',
+			'comments' => 'Comments:',
+		];
+
+		return Helpers::genericXpathParser($html, "//table//th", "//table//td", ' to ', $keyArray);
+
+	}
+
+}
