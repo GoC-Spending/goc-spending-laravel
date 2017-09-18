2b261d87853980115bd33b8cbb8ad0c960c1ce36
Sean Boots <sean.boots@gmail.com>
2017-09-18 00:03:31 -0400

    Added TC handler

---
+++ app/DepartmentHandlers/TcHandler.php
@@ -0,0 +1,54 @@
+<?php
+namespace App\DepartmentHandlers;
+
+use App\DepartmentHandler;
+use App\Helpers;
+
+class TcHandler extends DepartmentHandler {
+
+	public $indexUrl = 'http://wwwapps.tc.gc.ca/Corp-Serv-Gen/2/PDC-DPC/contract/reports.aspx';
+	public $baseUrl = 'http://wwwapps.tc.gc.ca/';
+	public $ownerAcronym = 'tc';
+
+	// From the index page, list all the "quarter" URLs
+	public $indexToQuarterXpath = "//form[@id='pageForm']//ul/li/a/@href";
+
+	public $multiPage = 0;
+
+	public $quarterToContractXpath = "//form[@id='pageForm']//table//td//a/@href";
+
+	
+	public function quarterToContractUrlTransform($contractUrl) {
+		return "http://wwwapps.tc.gc.ca/Corp-Serv-Gen/2/PDC-DPC/contract/" . $contractUrl;
+	}
+	
+
+	
+	public function indexToQuarterUrlTransform($url) {
+		return "http://wwwapps.tc.gc.ca/Corp-Serv-Gen/2/PDC-DPC/contract/" . $url;
+	}
+	
+
+	public $contractContentSubsetXpath = "//form[@id='pageForm']";
+
+	public function fiscalYearFromQuarterPage($quarterHtml) {
+
+		// //*[@id="pageForm"]/p[2]/strong
+
+		return Helpers::xpathRegexComboSearch($quarterHtml, "//form[@id='pageForm']/p[2]/strong", '/([0-9]{4})/');
+
+	}
+
+	public function fiscalQuarterFromQuarterPage($quarterHtml) {
+
+		return Helpers::xpathRegexComboSearch($quarterHtml, "//form[@id='pageForm']/p[2]/strong", '/([0-9])</');
+
+	}
+
+	public static function parseHtml($html) {
+
+		return Helpers::genericXpathParser($html, "//table//th[@scope='row']", "//table//td", ' to ');
+
+	}
+
+}
