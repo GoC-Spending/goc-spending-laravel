2a4d2ceeb1a2c144047f71b5c8da229bb9220582
Sean Boots <sean.boots@gmail.com>
2017-09-18 21:49:47 -0400

    Added INAC handler

---
+++ app/DepartmentHandlers/InacHandler.php
@@ -0,0 +1,42 @@
+<?php
+namespace App\DepartmentHandlers;
+
+use App\DepartmentHandler;
+use App\Helpers;
+
+class InacHandler extends DepartmentHandler {
+
+	public $indexUrl = 'http://www.aadnc-aandc.gc.ca/prodis/cntrcts/rprts-eng.asp';
+	public $baseUrl = 'http://www.aadnc-aandc.gc.ca/';
+	public $ownerAcronym = 'inac';
+
+	// From the index page, list all the "quarter" URLs
+	public $indexToQuarterXpath = "//div[@class='center']//ul/li/a/@href";
+
+	public $multiPage = 1;
+	public $quarterMultiPageXpath = "//div[@class='align-right size-small']/a/@href";
+
+	public $quarterToContractXpath = "//table[@class='widthFull TableBorderBasic']//td//a/@href";
+
+	public $contractContentSubsetXpath = "//div[@class='center']";
+
+	public function fiscalYearFromQuarterPage($quarterHtml) {
+
+		// <title>Disclosure of Contracts - 2016-2017 - 3rd Quarter - Indigenous and Northern Affairs Canada</title>
+		return Helpers::xpathRegexComboSearch($quarterHtml, "//title", '/([0-9]{4})/');
+
+	}
+
+	public function fiscalQuarterFromQuarterPage($quarterHtml) {
+
+		return Helpers::xpathRegexComboSearch($quarterHtml, "//title", '/-\s([0-9])[a-z]/');
+
+	}
+
+	public static function parseHtml($html) {
+
+		return Helpers::genericXpathParser($html, "//table[@class='widthFull TableBorderBasic']//th", "//table[@class='widthFull TableBorderBasic']//td", ' - ');
+
+	}
+
+}
