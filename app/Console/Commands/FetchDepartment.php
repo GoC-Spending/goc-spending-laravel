53b90dcbd360100a3f11dc5ba69655d8840706e6
Lucas Cherkewski <lucas@ecustom.ca>
2017-09-21 22:25:05 -0400

    add fetch department artisan command

---
+++ app/Console/Commands/FetchDepartment.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Symfony\Component\Debug\Exception\FatalThrowableError;
+
+class FetchDepartment extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'department:fetch {acronym : The acronym of the department to scrape.}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Fetch the contract HTML for a department.';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $department = $this->argument('acronym');
+
+        $departmentClass = 'App\\DepartmentHandlers\\' . ucfirst(strtolower($department)) . 'Handler';
+
+        $departmentHandler = new $departmentClass;
+        $departmentHandler->fetch();
+    }
+}
