50b7a92919e21d3bb06db7a49fdc4c1a00cecc81
Sean Boots <sean.boots@gmail.com>
2017-09-24 03:04:40 +0000

    New command to fetch and then parse a department in sequence.

---
+++ app/Console/Commands/RunDepartment.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class RunDepartment extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'department:run {acronym : The acronym of the department to scrape.}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Fetch and then parse the contract HTML for a department.';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $department = $this->argument('acronym');
+
+        $departmentClass = 'App\\DepartmentHandlers\\' . ucfirst(strtolower($department)) . 'Handler';
+
+        // Check to see if the DepartmentHandler exists. If not, bail gracefully!
+        if (! class_exists($departmentClass)) {
+            $this->error('No department handler for that department. Check if you’ve typo’ed the department acronym.');
+            return;
+        }
+
+        $departmentHandler = new $departmentClass;
+        $departmentHandler->fetch();
+
+        $departmentHandler->parseAll();
+    }
+}
